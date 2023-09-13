10661a62bbbfed36d874cbb780bfacdc3be0fbcc
Braspi <shutmer@gmail.com>
2023-09-12 22:09:03 -0700

    nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,17 @@
 <script>
     import { _ } from 'svelte-i18n'
+
+    function onScroll() {
+        let header = document.querySelector('.home main').getBoundingClientRect();
+        this.navDetached = window.pageYOffset < (header.height / 8)
+    }
 </script>
 
-<div class="w-full h-navHeight text-dark fixed top-0 z-10 transition bg-opacity-50 bg-dark">
-    <a href="#">
-        <img src="@/assets/icon/logo/LOGO2_alpha.png" class="absolute left-[calc(4%)] top-1/2 transform -translate-y-1/2 h-[calc(var(--navHeight) + 20px)] select-none md:left-0 md:h-[calc(var(--navHeight) - 10px)]">
+<div id class="w-full h-navHeight text-dark fixed top-0 z-10 transition bg-opacity-50 bg-dark">
+    <a href="/">
+        <img src="icon/logo/LOGO2_alpha.png" alt="logo" class="absolute top-0.5 -translate-y-2 select-none left-0 h-24 md:left-5 md:h-32">
     </a>
 </div>
 
 
-<!--    <title>{$_('page_title')}</title>-->
+<!--<div class="bg-amber-200 sm:bg-green-500 md:bg-black xl:bg-amber-950"> asdasdasd</div>-->
