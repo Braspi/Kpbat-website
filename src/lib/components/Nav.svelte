43e10eaf5f24df7002d637baf629bd1ec6c9746d
ponurakk <p0unrakg@int.pl>
2023-09-15 20:39:08 +0200

    Fixed some responsibility

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -12,9 +12,9 @@
 
 <div id="nav" class="w-full h-28 fixed top-0 z-10 transition duration-500 text-light {navDetachedClass}">
     <a href="/">
-        <img src="icon/logo/LOGO2_alpha.png" alt="logo" class="absolute top-0.5 -translate-y-2 select-none left-0 h-24 md:left-2 md:h-32">
+        <img src="icon/logo/LOGO2_alpha.png" alt="logo" class="absolute top-0.5 -translate-y-2 select-none left-0 h-24 md:left-1 md:h-28">
     </a>
-    <div class="hidden sm:block font-normal xl:flex flex-col items-center  h-full">
+    <div class="hidden font-normal md:flex flex-col items-center h-full">
         <div class="mt-auto mb-auto">
             <a href="/" class="mt-14 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.home')}</a>
             <a href="/" class="mt-14 ml-16 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.gallery')}</a>
