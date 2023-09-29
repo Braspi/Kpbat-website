7840fdbb9341aa31193462fc1653484efba14d17
ponurakk <p0unrakg@int.pl>
2023-09-29 21:25:08 +0200

    Fixed Nav Burger

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,7 +4,7 @@
     import { faBars } from '@fortawesome/free-solid-svg-icons';
 
     let navDetached = true;
-    let showBurgerDiv: boolean = false;
+    let showBurgerDiv: boolean = true;
 
     function onScroll() {
         let header = document.getElementById('nav')?.getBoundingClientRect() as DOMRect;
@@ -29,8 +29,8 @@
     <div class="hidden font-normal md:flex flex-col items-center h-full">
         <div class="mt-auto mb-auto">
             <a href="/" class="mt-14 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.home')}</a>
-            <a href="/" class="mt-14 ml-16 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.gallery')}</a>
-            <a href="/" class="mt-14 ml-16 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.contact')}</a>
+            <a href="/gallery" class="mt-14 ml-16 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.gallery')}</a>
+            <a href="/#contact" class="mt-14 ml-16 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.contact')}</a>
         </div>
         <div class="absolute right-8 h-24 flex justify-around w-28 md:h-28 md:top-9">
             <div role="button" tabindex="0" on:click={() => changeLang("fr")} on:keypress={() => changeLang("fr")} class="w-14 h-9 p-1.5 cursor-pointer select-none hover:brightness-50">
@@ -47,15 +47,15 @@
     </div>
 
     {#if showBurgerDiv}
-        <div class="w-screen h-screen bg-dark absolute z-30">
-            <div class="flex flex-col m-auto w-max text-center absolute bottom-1/2 left-1/2 -translate-x-1/2">
-                <a href="/" class="mt-10 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.home')}</a>
-                <a href="/" class="mt-10 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.gallery')}</a>
-                <a href="/" class="mt-10 text-2xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.contact')}</a>
+        <div class="w-screen h-screen bg-dark absolute z-30 flex flex-col items-center justify-around">
+            <div class="flex flex-col w-max text-center gap-7">
+                <a href="/" class="mt-10 text-4xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.home')}</a>
+                <a href="/gallery" class="mt-10 text-4xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.gallery')}</a>
+                <a href="/#contact" class="mt-10 text-4xl no-underline uppercase hover:text-hoverColor transition duration-100">{$_('nav.contact')}</a>
             </div>
-            <div class="absolute bottom-10 left-1/2 -translate-x-1/2">
+            <div class="">
                 <h2 class="text-3xl font-semibold text-center mb-10">{$_('nav.language')}:</h2>
-                <div class="h-24 flex justify-around w-96 md:h-28 md:top-9">
+                <div class="h-24 flex justify-around w-96">
                     <div role="button" tabindex="0" on:click={() => changeLang("fr")} on:keypress={() => changeLang("fr")} class="w-auto h-20 p-1.5 cursor-pointer select-none hover:brightness-50">
                         <img src="icon/flags/france.webp" alt="france-flag" class="w-full h-full">
                     </div>
@@ -70,5 +70,3 @@
 
 <!-- Attach scroll to document -->
 <svelte:document on:scroll={onScroll} />
-
-<!--<div class="bg-amber-200 sm:bg-green-500 md:bg-black xl:bg-amber-950"> asdasdasd</div>-->
