4627fbe62dcd7674584295c5e7d81dd51baa1531
ponurakk <p0unrakg@int.pl>
2023-10-03 22:08:43 +0200

    Added Services Section

---
+++ src/lib/utils/getSections.ts
@@ -0,0 +1,23 @@
+import { faDungeon, faHome, faToolbox } from "@fortawesome/free-solid-svg-icons";
+
+export const sections = [
+  {
+    id: 1,
+    icon: faHome,
+    text: "services.sections.exterior",
+    hovered: false,
+  },
+  {
+    id: 2,
+    icon: faDungeon,
+    text: "services.sections.interior",
+    hovered: false,
+  },
+  {
+    id: 3,
+    icon: faToolbox,
+    text: "services.sections.energy",
+    hovered: false,
+  }
+]
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,11 @@
     import Nav from "$lib/components/Nav.svelte";
     import Footer from "$lib/components/Footer.svelte";
     import FormSection from "$lib/components/sections/FormSection.svelte";
+    import ServicesSection from "$lib/components/sections/ServicesSection.svelte";
 </script>
 
 <Nav />
 <slot />
+<ServicesSection />
 <FormSection/>
 <Footer />
