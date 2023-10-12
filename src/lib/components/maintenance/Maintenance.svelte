16572c9b36f6269945c8fafaa5db9b2d1c7333f3
Braspi <shutmer@gmail.com>
2023-10-12 22:02:54 +0200

    maintenance

---
+++ src/lib/components/maintenance/Maintenance.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<section class="bg-light min-h-screen flex items-center justify-center">
+  <h1 class="text-2xl"> <a href="https://ycode.pl" class="underline"> ycode.pl </a> | Website under construction...</h1>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
-<script>
+<script lang="ts">
     import "../app.css";
     import '@splidejs/svelte-splide/css';
     import "../i18n.ts";
 
     import Nav from "$lib/components/Nav.svelte";
     import Footer from "$lib/components/Footer.svelte";
+    import Maintenance from "$lib/components/maintenance/Maintenance.svelte";
+    import {onMount} from "svelte";
+
+    let isMaintenance = true;
+    onMount(()=>{
+        isMaintenance = localStorage.getItem("devMode") == null;
+    })
+
 </script>
 
-<Nav />
-<slot />
-<Footer />
+
+{#if !isMaintenance}
+  <Nav />
+  <slot />
+  <Footer />
+{:else }
+  <Maintenance />
+{/if}
