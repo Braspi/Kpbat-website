987913b9f16ca8e6fb998918838b08de7e7a8f12
ponurakk <p0unrakg@int.pl>
2023-10-16 22:25:03 +0200

    temporary fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+// FIX: this is bad and shouldn't be done in that way
+let galleryEntries = [];
+
+for (let i = 0; i < 49; i++) {
+	galleryEntries.push(`/gallery/${i+1}`);
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -13,11 +20,15 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
+			crawl: true,
 			entries: [
 				"/",
 				"/gallery",
-				"/gallery/[id]"
+				...galleryEntries,
 			]
+		},
+		paths: {
+			base: ""
 		}
 	}
 };
