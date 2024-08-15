844e9931cb422323dd1c51722405f77f5e750208
Braspi <shutmer@gmail.com>
2024-08-15 14:32:44 +0200

    inital

---
+++ components/Nav.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const links = [
+  {
+    name: "Accueil",
+    path: "/"
+  },
+  {
+    name: "Galerie",
+    path: "/"
+  },
+  {
+    name: "Contact",
+    path: "/"
+  }
+]
+
+</script>
+
+<template>
+  <section class="w-full flex justify-center items-center ">
+    <NuxtLink
+        v-for="{name, path} in links"
+        :to="path"
+        class="px-4"
+    >
+      {{ name }}
+      na
+    </NuxtLink>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+import Nav from "~/components/Nav.vue";
+</script>
+
+<template>
+  <Nav/>
+</template>
